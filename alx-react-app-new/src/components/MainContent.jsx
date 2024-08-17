9c56ec0ae0eb55bef106ed00f4c738ee09468c53
ayiekonick <ayiekonick254@gmail.com>
2024-08-17 23:16:31 +0300

    added inline css

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return(
+    <main style={{backgroundColor:'green',color:'violet',textAlign:'center',fontSize:'25px'}}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+    );
+}
+
+export default MainContent;
