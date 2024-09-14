f57a10fd139e833ea2015634c4199979536eabea
ayiekonick <ayiekonick254@gmail.com>
2024-09-14 17:36:04 +0300

    included shadow utility to RecipeDetail

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -17,8 +17,8 @@ function RecipeDetail() {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-6">
-      <img src={recipe.image} alt={recipe.title} className="w-full h-64 object-cover rounded-lg mb-6" />
+    <div className="max-w-4xl mx-auto p-6 shadow-lg"> {/* Added shadow-lg here */}
+      <img src={recipe.image} alt={recipe.title} className="w-full h-64 object-cover rounded-lg mb-6 shadow" /> {/* Added shadow here */}
       <h1 className="text-3xl font-bold mb-4">{recipe.title}</h1>
       <p className="text-gray-700 text-lg mb-6">{recipe.summary}</p>
       
